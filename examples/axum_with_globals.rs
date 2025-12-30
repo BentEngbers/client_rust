81d23305128975da2c13a93f3f2f2cf4fa324ca3
Bent Engbers <bentengbers@dreamsolution.nl>
2025-12-30 15:43:59 +0100

    Remove seperate module
    Signed-off-by: Bent Engbers <bentengbers@dreamsolution.nl>

--- examples/axum_with_globals.rs
+++ examples/axum_with_globals.rs
@@ -1,53 +1,49 @@
-use axum::response::IntoResponse;
-use axum::routing::get;
-use axum::Router;
-use prometheus_client::metrics::counter::Counter;
-use prometheus_client::metrics::family::Family;
+use axum::{
+    body::Body,
+    http::{header::CONTENT_TYPE, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use prometheus_client::{
+    encoding::text::encode,
+    metrics::{counter::Counter, family::Family},
+    registry::{Metric, Registry},
+};
 use prometheus_client_derive_encode::{EncodeLabelSet, EncodeLabelValue};
-use std::sync::LazyLock;
+use std::sync::{LazyLock, Mutex};
 
-//A separate module (usually in a seperate file) to scope the registry
-mod metrics {
-    use axum::body::Body;
-    use axum::http::header::CONTENT_TYPE;
-    use axum::http::StatusCode;
-    use axum::response::{IntoResponse, Response};
-    use prometheus_client::encoding::text::encode;
-    use prometheus_client::registry::{Metric, Registry};
-    use std::sync::{LazyLock, Mutex};
+static REGISTRY: LazyLock<Mutex<Registry>> = LazyLock::new(|| Mutex::new(Registry::default()));
 
-    static REGISTRY: LazyLock<Mutex<Registry>> = LazyLock::new(|| Mutex::new(Registry::default()));
+fn register_metric_to_global_registry<MetricType: Metric + Clone + Default>(
+    name: &str,
+    help: &str,
+) -> MetricType {
+    let metric: MetricType = MetricType::default();
+    let mut registry = REGISTRY.lock().expect(&format!(
+        "Cannot lock metrics registry to create {name} metric"
+    ));
+    registry.register(name, help, metric.clone());
+    metric
+}
 
-    pub fn register_metric_to_global_registry<MetricType: Metric + Clone + Default>(
-        name: &str,
-        help: &str,
-    ) -> MetricType {
-        let metric: MetricType = MetricType::default();
-        let mut registry = REGISTRY.lock().expect(&format!(
-            "Cannot lock metrics registry to create {name} metric"
-        ));
-        registry.register(name, help, metric.clone());
-        metric
+async fn metrics_handler() -> impl IntoResponse {
+    let mut buffer = String::new();
+    {
+        let registry = REGISTRY
+            .lock()
+            .expect("could not acquire a lock on registry to push metrics");
+        encode(&mut buffer, &registry).unwrap();
     }
 
-    pub async fn metrics_handler() -> impl IntoResponse {
-        let mut buffer = String::new();
-        {
-            let registry = REGISTRY
-                .lock()
-                .expect("could not acquire a lock on registry to push metrics");
-            encode(&mut buffer, &registry).unwrap();
-        }
-
-        Response::builder()
-            .status(StatusCode::OK)
-            .header(
-                CONTENT_TYPE,
-                "application/openmetrics-text; version=1.0.0; charset=utf-8",
-            )
-            .body(Body::from(buffer))
-            .unwrap()
-    }
+    Response::builder()
+        .status(StatusCode::OK)
+        .header(
+            CONTENT_TYPE,
+            "application/openmetrics-text; version=1.0.0; charset=utf-8",
+        )
+        .body(Body::from(buffer))
+        .unwrap()
 }
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
@@ -62,7 +58,7 @@ pub struct MethodLabels {
 }
 
 static METRIC: LazyLock<Family<MethodLabels, Counter>> =
-    LazyLock::new(|| metrics::register_metric_to_global_registry("requests", "Count of requests"));
+    LazyLock::new(|| register_metric_to_global_registry("requests", "Count of requests"));
 
 pub async fn some_handler() -> impl IntoResponse {
     METRIC
@@ -76,7 +72,7 @@ pub async fn some_handler() -> impl IntoResponse {
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-        .route("/metrics", get(metrics::metrics_handler))
+        .route("/metrics", get(metrics_handler))
         .route("/handler", get(some_handler));
     let port = 8080;
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
